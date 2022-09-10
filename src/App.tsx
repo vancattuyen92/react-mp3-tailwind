d186a919607bf26d31639ed632870bc5cada0caa
Tony Nguyen <nhattruong1811@gmail.com>
2022-09-10 11:07:53 +0700

    push code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="font-bold underline text-[100px]">
-        Hello world!
-      </h1>
+      <aside className='relative z-[100] w-60 h-screen bg-slate-300'>aside</aside>
 
+      <header  className='relative z-[100] w- h-screen bg-slate-300'>header</header>
+
+      <main>main</main>
     </div>
   );
 }
