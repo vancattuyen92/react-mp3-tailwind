f10d6a2fa30f6d3f92fa9783c14ee36b1ceffb26
Tony Nguyen <nhattruong1811@gmail.com>
2022-09-18 11:53:32 +0700

    Delete code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-  
+  console.log('truong')
+  console.log('tien')
+  console.log('nguyen')
   return (
     <div className="App">
       <aside className='relative z-[100] w-60 h-screen bg-slate-300'>
